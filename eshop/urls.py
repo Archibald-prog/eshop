71062136e0d4dc339dd872b95c1c49af0602863b
[Archibald] <[archpod@gmail.com]>
2025-09-01 14:21:56 +0300

    Added ProductListView and ProductDetailView

--- eshop/urls.py
+++ eshop/urls.py
@@ -2,12 +2,13 @@
 URL configuration for eshop project.
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include("products.urls")),
 ]
 
 if settings.DEBUG:
--- products/views.py
+++ products/views.py
@@ -1,3 +1,43 @@
-from django.shortcuts import render
+import random
+from django.views.generic import ListView, DetailView
+from products.models import Product
 
-# Create your views here.
+
+def get_random_id(recommended=True):
+    if recommended:
+        initial_qs = Product.objects.filter(is_recommended=True)
+    else:
+        initial_qs = Product.objects.filter(is_new=True)
+    random_list = random.sample(list(initial_qs), 8)
+    random_id = [obj.id for obj in random_list]
+    return random_id
+
+
+class ProductListView(ListView):
+    model = Product
+
+    def get_queryset(self):
+        id_list = get_random_id()
+        return Product.objects.filter(pk__in=id_list)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "товары/главная"
+        id_list = get_random_id(recommended=False)
+        new_products = Product.objects.filter(pk__in=id_list)
+        context["new_products"] = new_products
+        return context
+
+
+class ProductDetailView(DetailView):
+    model = Product
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["title"] = "товары/товар"
+        obj = context["object"]
+        features = obj.productfeatures_set.all().first()
+        same_products = Product.objects.filter(category=obj.category).exclude(pk=obj.pk)
+        context["same_products"] = same_products
+        context["features"] = features
+        return context
