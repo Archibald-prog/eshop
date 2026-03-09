4c41745ec965770885455e4ae9f786f50e04b403
[Archibald] <[archpod@gmail.com]>
2026-03-09 09:28:27 +0300

    chore(settings): implement environment-based static configuration

--- eshop/settings.py
+++ eshop/settings.py
@@ -113,10 +113,14 @@ USE_TZ = True
 
 FORCE_SCRIPT_NAME = env('FORCE_SCRIPT_NAME')
 STATIC_URL = f'{FORCE_SCRIPT_NAME}/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, "static"),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+if DEBUG:
+    STATICFILES_DIRS = (
+        os.path.join(BASE_DIR, "static"),
+    )
+else:
+    STATICFILES_DIRS = []
 
 MEDIA_URL = f'{FORCE_SCRIPT_NAME}/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
